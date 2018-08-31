b7cd05c64e67c8489697f0b9c076d4e540f93f24
TJVictor <guoyongcheng_61@163.com>
2018-08-31 21:05:12 +0800

    HOME

---
+++ src/main/java/microYoga/rest/wxServices.java
@@ -0,0 +1,47 @@
+package microYoga.rest;
+
+import microYoga.utils.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/wxServices")
+public class wxServices {
+
+    //region private
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping(value = "login",method= RequestMethod.GET)
+    public void login(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("success");
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            if(CommonUtils.checkSignature(signature, timestamp, nonce)){
+                out.write(echostr);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            out.close();
+        }
+
+    }
+
+
+}
